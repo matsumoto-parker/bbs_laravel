c1e2aeb4c9758bf006f2a20d8e49e5401838b44b
Matsumoto Miu <miu.matsumoto04@gmail.comgit config --global user.email miu.matsumoto04@gmail.com>
2020-06-05 18:04:36 +0900

    feat: add store.php

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -4,20 +4,27 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <h1>New Post page</h1>
-            <div class="card text-center">
-                <div class="card-header">
-                    bbs
+            @if ($errors->any())
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
                 </div>
-                <div class="card-body">
-                    <h5 class="card-title">title：</h5>
-                    <p class="card-text">content：</p>
-                    <a href="#" class="btn btn-primary">詳細</a>
+            @endif
+            <form action="{{ route('posts.store') }}" method="POST">
+            {{csrf_field()}}
+                <div class="form-group">
+                    <label>タイトル</label>
+                    <input type="text" class="form-control" placeholder="タイトルを入力して下さい" name="title">
                 </div>
-                <div class="card-footer text-muted">
-                    posted at：
+                <div class="form-group">
+                    <label>内容</label>
+                    <textarea class="form-control" placeholder="内容" rows="5" name="body"></textarea>
                 </div>
-            </div>
+                <button type="submit" class="btn btn-primary">作成する</button>
+            </form>
         </div>
     </div>
 </div>
